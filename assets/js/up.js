1e31b1e9670d40df9c3ec49441c9dc8249b639e6
Tom Vieillard--Grelet <52112041+vgtom4@users.noreply.github.com>
2022-09-03 15:27:45 +0000

    Edit page

---
+++ assets/js/up.js
@@ -0,0 +1,11 @@
+//button up
+var mybutton = document.getElementById("up");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
