518063e84700365e22a6a3cf9fb74eb988246087
Tom Vieillard--Grelet <52112041+vgtom4@users.noreply.github.com>
2023-08-26 10:02:04 +0000

    Modification du js pour le modal de projet + déplacement les infos de projet dans un fichier à part. - les informations supplémentaires des projets ont déplacées dans projetslist.js. - il n'y a désormais plus qu'un <dialog> dans le fichier HTML principal à la place d'en avoir un pour chaque projet.

---
+++ assets/js/projectslist.js
@@ -0,0 +1,19 @@
+const projectTemplates = {
+    project1: `
+        <div>
+            <img src="https://gyazo.com/a0f6ef75502ff05ad78e7778c5235ee7.gif" alt="Projet 1" title="c'est magnifique" />
+            <h2>Projet 1</h2>
+            <p>Description du projet 1.</p>
+            <!-- ... d'autres informations ... -->
+        </div>
+    `,
+    project2: `
+        <div>
+            <img src="https://gyazo.com/f34b0ed1f228f5b8106acd7ff5637ce0.gif" alt="Projet 2" />
+            <h2>Projet 2</h2>
+            <p>Description du projet 2.</p>
+            <!-- ... d'autres informations ... -->
+        </div>
+    `,
+    // ... autres projets ...
+};
